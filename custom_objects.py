a04cfc4815170131d8ca8a616b75997356e9ae50
Razzorior <christopher94.olson@gmail.com>
2024-06-27 15:12:27 +0200

    Adds more models and precalculations with injected errors.

--- custom_objects.py
+++ custom_objects.py
@@ -17,3 +17,12 @@ def custom_dead_relu_initializer(shape, dtype=None, partition_info=None):
     weights = tf.random.shuffle(weights, seed=42)
 
     return weights
+
+
+class GradientMonitor(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        # Get the gradients of all the trainable weights
+        for layer in self.model.layers:
+            if hasattr(layer, 'kernel'):
+                grads = self.model.optimizer.get_gradients(logs['loss'], layer.kernel)
+                print(f'Gradients for layer {layer.name}: {grads}')
